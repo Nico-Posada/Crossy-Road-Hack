73b56646d1e28885ef381a4f6791539d15bc262f
Nico-Posada <102486290+Nico-Posada@users.noreply.github.com>
2023-01-01 05:04:45 -0600

    Update Program.cs

--- Crossy Road Test/Program.cs
+++ Crossy Road Test/Program.cs
@@ -1,58 +1,135 @@
-﻿using System;
+using Memory;
+using System;
+using System.Linq;
 using System.Threading;
 
 namespace Crossy_Road_Test
 {
-    internal class Program
+    public class Program
     {
+        static readonly Mem m = new Mem();
+
+        private struct Offsets
+        {
+            public static long score_function    { get; set; }
+            public static long user_info         { get; set; }
+
+            public const int instruction_offset  = 0x06;
+            public const int coin_offset         = 0xC4;
+        }
+
+        private static bool Init()
+        {
+            // Scan for our addresses
+            var score_func_scan = m.FindBytes("55 8B EC 56 8B F1 ? ? ? ? ? ? ? ? 89 86 ? ? ? ? 8B 8E ? ? ? ? 3B C8", m.BaseAddress + 0x14F0000, m.BaseAddress + 0x1580000, true).FirstOrDefault();
+            var user_info_scan = m.FindBytes("B8 ? ? ? ? 8B 08 80 39 00 8B 16 E8", m.BaseAddress + 0x14F0000, m.BaseAddress + 0x1580000, true).FirstOrDefault();
+
+            // check to make sure scans were found
+            if (user_info_scan == default || score_func_scan == default)
+            {
+                Console.WriteLine("Something went wrong with the pattern scan.");
+                Thread.Sleep(4000);
+                return false;
+            }
+
+            // set struct values
+            Offsets.score_function = score_func_scan + Offsets.instruction_offset;
+            Offsets.user_info = m.Read<int>(user_info_scan + 1); // resolving mov instruction (x86 so you just read the 4 bytes after the instruction)
+
+            // make sure those modified values make sense (should almost always be fine)
+            bool result = m.IsValidAddr(Offsets.score_function) && m.IsValidAddr(Offsets.user_info);
+
+            if (!result)
+            {
+                Console.WriteLine("Unknown error occured.");
+                Thread.Sleep(4000);
+            }
+
+            return result;
+        }
+
+        // When hack is inactive, the first two bytes of the address will be 0x8B 0x86.
+        // If you convert that to ushort, you get 34443, so if we read ushort
+        // On that address and it isnt 34443, then we know the hack is active.
+        private static bool IsActive(long addr) => m.Read<ushort>(addr) != 34443;
+
         static void Main()
         {
-            Memory.Mem m = new Memory.Mem();
-            Start: Console.Clear();
-            Console.Write("Crossy Road Instant Max Score Hack\n[1] Activate\n[2] Deactivate\n\nInput: ");
-            string read = Console.ReadLine();
-            bool? getResponse = read == "1" ? true : read == "2" ? false : null;
-
-            if (m.IsProcessWithModulesRunning("Crossy Road", "Crossy Road.dll"))
-            {
-                // scan for xref to the main function I'm writing the code cave in (using xref for consistent sig that hopefully wont die on updates)
-                long[] addr = m.FindBytes(new byte?[] { 0xE8, null, null, null, null, 0xE9, null, null, null, null, 0xBA, null, null, null, null, 0xFF, 0x15, null, null, null, null, 0xB8, null, null, null, null, 0x8B, 0xF8 }, m.BaseAddress + 0x14F0000, m.BaseAddress + 0x1580000, true);
-                if (m.IsValidAddr(addr[0]))//           E8 ? ? ? ? E9 ? ? ? ? BA ? ? ? ? FF 15 ? ? ? ? B8 ? ? ? ? 8B F8  -  if game updates and the tool doesnt work, search for this sig and make sure that 1) its not dead and 2) it falls into the pattern scan range
-                {
-                    m.universalResolve(ref addr[0], 1, 0x80); // Resolve, added 0x80 because I'm not going to the start of the function rather to the trampoline
-                    switch (getResponse)
-                    {
-                        case true:
-                            long trampolineTo = addr[0] + 0x56;
-                            m.WriteXBytes(addr[0], new byte[] { 0xEB, 0x54 }); // jmp to code cave
-                            m.WriteXBytes(trampolineTo, new byte[] { 5, 0x0F, 0x27, 0, 0, 0xE9 }); // assembly code (ew)
-                            m.WriteInt(trampolineTo + 6, (int)(addr[0] - trampolineTo - 8));  // Calculate and write relative address
-                            Console.WriteLine("\nSuccessfully Activated!");
-                            Thread.Sleep(4000);
-                            goto Start; // We did it! Go back to the start (:
-                        case false:
-                            m.WriteXBytes(addr[0], new byte[] { 3, 0xC2 }); // This is the original code that was overwriten, just reverts it so it doesnt redirect to our cave
-                            Console.WriteLine("\nSuccessfully Deactivated!");
-                            Thread.Sleep(4000);
-                            goto Start;
-                        default:
-                            Console.WriteLine("\nPlease Input Either a 1 or a 2 don't be dumb.");
-                            Thread.Sleep(4000);
-                            goto Start;
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("Something went wrong with the pattern scan.");
-                    Thread.Sleep(4000);
-                    goto Start;
-                }
-            }
-            else
+            if (!m.IsProcessRunning("Crossy Road", "Crossy Road.dll"))
             {
                 Console.WriteLine("Process not found.");
                 Thread.Sleep(4000);
-                goto Start;
+                return;
+            }
+
+            if (!Init())
+                return;
+
+            while (true)
+            {
+                Console.Clear();
+                Console.Write("Crossy Road Console Menu:\n[1] Max Score Hack\n[2] Set Coin Count\n\nInput: ");
+                string read = Console.ReadLine();
+                
+                // Make sure input is valid (is it a 1 or a 2)
+                if (!int.TryParse(read, out int input) || 0 >= input || input >= 3)
+                    continue;
+
+                switch (input)
+                {
+                    case 1:
+                        bool active = IsActive(Offsets.score_function);
+                        Console.Clear();
+                        Console.Write("Max Score Hack\nHack: ");
+                        Console.ForegroundColor = active ? ConsoleColor.Green : ConsoleColor.Red;
+                        Console.WriteLine(active ? "Active" : "Inactive");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine("\nPress any key to toggle hack...");
+                        Console.ReadKey();
+                        
+                        // write modified bytes
+                        if (!active) m.WriteBytes(Offsets.score_function, new byte[] { 0xB8, 0x0F, 0x27, 0x00, 0x00, 0x90, 0x90, 0x90 });
+                        // ASSEMBLY
+                        //  mov eax, 9999
+                        //  nop
+                        //  nop
+                        //  nop
+                        
+                        // restore bytes
+                        else m.WriteBytes(Offsets.score_function, new byte[] { 0x8B, 0x86, 0xA4, 0x00, 0x00, 0x00, 0x03, 0xC2 });
+                        // ASSEMBLY
+                        //  mov eax,[esi+000000A4]
+                        //  add eax,edx
+
+                        Console.WriteLine("Successfully toggled!");
+                        Thread.Sleep(4000);
+
+                        break;
+                    case 2:
+                        while (true)
+                        {
+                            Console.Clear();
+                            Console.Write("Set Coin Count\nInput: ");
+                            string coin_str = Console.ReadLine();
+
+                            if (!int.TryParse(coin_str, out int coins))
+                                continue;
+                            
+                            // get encrypted coin value, then write it to memory
+                            // (read user info offset because it's a pointer)
+                            
+                            int enc_coin_value = (coins + 0x4E876) ^ 0x4E876; // the reverse of the crappy encryption method crossy road uses
+                            m.Write(m.Read<int>(Offsets.user_info) + Offsets.coin_offset, enc_coin_value);
+                            break;
+                        }
+
+                        Console.WriteLine("Successfully set coins!");
+                        Thread.Sleep(4000);
+
+                        break;
+                    default:
+                        break;
+                }
             }
         }
     }
